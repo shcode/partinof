3f3bfd8b669d5c8fa3f10d0cdc3640dfbf9d43f7
M. Sulton Hasanuddin <shcode@ymail.com>
2019-02-23 21:12:13 +0700

    add devise integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  include ActionController::MimeResponds
+  respond_to :json
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password, :password_confirmation, :first_name, :last_name)
+    end
+  end
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    render_resource(resource)
+  end
+end
